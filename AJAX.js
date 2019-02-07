e4f989f7731337542c1c4fc99be9fc196fbebe26
Xiaolu Wang <xiaolu.wang.18@ucl.ac.uk>
2019-02-07 16:11:37 +0000

    error: Cannot set property 'innerHTML' of null at XMLHttpRequest.processDivNodeJSChange

--- AJAX.js
+++ AJAX.js
@@ -1,11 +1,8 @@
-// define the global variable to process the AJAX request
 var xhrNode;
-
 
 function callDivNodeJSChange() {
     xhrNode = new XMLHttpRequest();
-    // get the content of the text box and send it to the server
-    var url = "http://developer.cege.ucl.ac.uk:"+httpPortNumber;
+    var url = "http://developer.cege.ucl.ac.uk:" + httpPortNumber;
     xhrNode.open("GET", url, true);
     xhrNode.onreadystatechange = processDivNodeJSChange;
     try {
@@ -20,12 +17,12 @@ function processDivNodeJSChange() {
     // while waiting response from the server
     if (xhrNode.readyState < 4) {
         document.getElementById('ajaxtext').innerHTML = "Loading...";
-    }
-    // 4 = Response from server has been completely
-    else if (xhrNode.readyState === 4) {
-        // http status between 200 to 299 are all successful
-        if (xhrNode.status == 200 && xhrNode.status < 300) {
-            document.getElementById('ajaxtext').innerHTML = xhrNode.responseText;
+    } else {
+        // 4 = Response from server has been completely loaded.
+        if (xhrNode.readyState === 4) {
+            // http status between 200 to 299 are all successful
+            if (xhrNode.status == 200 && xhrNode.status < 300)
+                document.getElementById('ajaxtext').innerHTML = xhrNode.responseText;
         }
     }
 }
