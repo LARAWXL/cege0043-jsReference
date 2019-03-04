52bca923ca4469e6ad1389be6981fbaef98b42b7
Xiaolu Wang <xiaolu.wang.18@ucl.ac.uk>
2019-03-04 14:49:03 +0000

    Using the Layer for Distance Measurement

--- leafletFunctions.js
+++ leafletFunctions.js
@@ -47,8 +47,7 @@ function getDistanceFromPoint(position) {
     // and pop up an alert message.
     if (distance < 0.1) {
         alert("You are within 100m of Warren Street Station!!")
-    }
-    else {
+    } else {
         alert("You are at " + distance + " km away from Warren Street Station!! ")
     }
 }
@@ -96,4 +95,35 @@ function getDistanceFromMultiplePoints(position) {
         }
     }
     alert("Earthquake: " + closestQuake + " is distance " + minDistance + "away");
+}
+
+
+// take the leaflet formdata layer (in xhrFormData.js)
+// go through each point one by one
+// and measure the distance to Warren Street
+// for the closest point show the pop up of that point
+function closestFormPoint() {
+    var minDistance = 100000000000;
+    var closestFormPoint = 0;
+    // for this example, use the latitude/longitude of warren street
+    // in your assignment replace this with the user's location
+    var userlat = 51.524048;
+    var userlng = -0.139924;
+    formLayer.eachLayer(function (layer) {
+        var distance = calculateDistance(userlat,
+            userlng, layer.getLatLng().lat, layer.getLatLng().lng, 'K');
+        if (distance < minDistance) {
+            minDistance = distance;
+            closestFormPoint = layer.feature.properties.id;
+        }
+    });
+    // for this to be a proximity alert, the minDistance must be
+    // closer than a given distance - you can check that here
+    // using an if statement
+    // show the popup for the closest point
+    formLayer.eachLayer(function (layer) {
+        if (layer.feature.properties.id == closestFormPoint) {
+            layer.openPopup();
+        }
+    });
 }
